7c27d1b6f62a979feda9fe1ef15582f5fdc832b0
FATIMA.A.R <iamqasim221@gmail.com>
2024-11-27 23:15:13 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,277 @@
+
+/// TASK##NO.1
+//how to done the task to enter any name of city and get its's name access by its speciality????
+
+
+// var arrycity = ['karachi','peshawar','multan','lahore','islamabad']
+// var inp = prompt('enter your city name')
+// if (inp== 'karachi'){
+//     document.write('welcome to the city of lights')
+// }
+// else if (inp=="peshawar"){
+//     document.write('welcome to peshawar')
+// }
+// else{
+//     document.write('invalid city name')
+// }
+
+
+
+
+
+
+
+/// TASK##NO.2
+// HOW TO TAKE GENDER PR0MPT AND BY HAVING FEMAIL OR MALE WELCOME THEM AS WELCOME SIR OR MAAM~??
+
+
+// var userinp = prompt('enter your gender')
+// var male = "male"
+// var female= "female"
+// if    (userinp==male){
+//     alert("welcome to our page sir")
+// }
+// else if ( userinp== female){
+//     alert("welcome to our website ma'am" )
+// }
+// else {
+//     alert("inavlid gender please try again")
+// }
+
+
+
+
+
+
+
+
+
+/// TASK##NO.3
+// how to write a program to take traffic color's name and according to color guide them the rule of it?
+
+// var inp = prompt('enter traffic color name')
+// var color1 = "red"
+// var color2 = "yellow"
+// var color3 = "green"
+// if (inp== color1){
+//     alert(inp+ " "+` -> MUST STOP`)
+// }
+// else if  (inp== color2){
+//     alert(inp+ " "+` -> READY  TO MOVE`)
+// }
+// else if  (inp== color3){
+//     alert(inp+ " "+` ->  MOVE now`)
+// }
+// else {
+//     alert ( ' invalid trafic color plz try again')
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/// TASK##NO.4
+// how to write a program to take  an input remaining fuel in car if it is less than 0.25 then  show them a text to refill the fuel in your car?
+
+
+// var inp = prompt('enter your  remaining fuel in liters!')
+// for ( var i=0; i<=25; i++){
+//     if ( inp== i ){
+//         alert('please refill the fuel in your car!')
+//     }
+
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+/// TASK##NO.5
+//store a secret no from 1-10 in a variable ~ prompt user to get a secret no guess..
+//a. if corrcet --- Bing0! Correct answer!
+//b. if +1 to secret code then show 
+// 'close enough to the correct answer'
+// var inp = +prompt('GUESS ANY NUMBER')
+//     if ( inp== 1){
+//         alert('wrong guess try again')
+//     }
+//     else if ( inp== 2){
+//         alert('wrong guess try again')
+//     }
+//     else if ( inp== 3){
+//         alert('wrong guess try again')
+//     }
+//     else if ( inp== 4){
+//         alert('wrong guess try again')
+//     }
+//     else if ( inp== 5){
+//         alert('wrong guess try again')
+//     }
+//     else if ( inp== 6){
+//         alert('wrong guess  but you are  near to reach!please try again')
+//     }
+//     else if ( inp== 7){
+//         alert('wrong guess  but you are  near to reach!please try again')
+//     }else if ( inp== 8){
+//         alert('wrong guess  but you are close enough to the correct answer!')
+//     }
+// else if ( inp==9){
+//     alert('congratulations you have a right one guess   you win  ')
+// }lert('sorry your suggestion is invalid')
+// }
+    
+
+
+
+
+
+
+
+// TASK##NO.6
+//c/heck whether the no is divisible by 3 or not of if yes then alert the user!
+// var dd=+prompt('enter any no you wanna check which is divisible by 3 or not!')
+// // var arry=[3,6,9,12,15,18,21,24,27,30,] 
+
+// if (dd== 3||6||9||12||15||18||21||24||27||30){
+//     document.write(dd+' ' + 'is divisible by 3')
+// }
+
+//  if ( dd  3||6||9||12||15||18||21||24||27||30)  { 
+//     document.write(`${dd}  is n0t divisble  by 3`)
+// }
+
+
+
+
+
+
+
+// / TASK##NO.7
+// var pro = prompt('enter any  no to check whether the no is even 0r odd!')
+// for (var i=0; i<=1000; i=i+2){
+//  if (pro==i){
+//     alert(`${pro} is an even number`)
+//  }
+// }
+// for (var j=1; j<=1000; j=j+2){
+//     if (pro==j){
+//        alert(`${pro} is an odd number`)
+//     }
+//    }
+        
+    
+
+
+
+
+
+// / TASK##NO.8
+// / take temperature as an input and show
+// t>40 too hot
+// t>30  normal
+// t>20 cool
+// t>10 omg so cool  
+// var inp= prompt("enter your area's temperature")
+// match=false
+// for (var i=-10; i<=10.; i++){
+//     if (inp==i){
+//         alert('OMG! TODAYS WEATHER IS SO COOL.')
+//     }
+// }
+// for (var j=11; j<=20.; j++){
+//     if (inp==j){
+//         alert(' TODAYS WEATHER IS  COOL.')
+//     }
+// }
+// for (var k=20; k<=30; k++){
+//     if (inp==k){
+//         alert(' THE WEATHER  TODAY IS NORMAL.')
+//     }
+// }
+
+// for (var L=30; L<=40.; L++){
+//     if (inp==L){
+//         alert('IT IS TOO HOT OUTSIDE!')
+//     }
+    
+// }
+
+
+// if (       
+// match=true
+//   ){
+// alert(" sorry! you've entered wrong number")
+//   }
+
+
+
+
+
+
+
+
+
+// / TASK##NO.9
+//calculator//
+// function getans(){
+//     var b = document.getElementById("three")
+//     b.value = eval(b.value)
+// }
+//  function getvalue(a) {
+//     var b = document.getElementById('three')
+//     b.value +=a
+// }
+
+
+
+
+// / TASK##NO.10
+// var arry= ['saleem','arbaz','salman']
+// var arry2=['father' , 'mother' ]
+
+// for ( var i=0; i<arry.length; i++){
+//     for ( j=0; j<arry2.length; j++){
+//         document.write( arry[i] + " " + arry2[j] + " <br>")
+//     }
+// }
+
+
+
+// / TASK##NO.11
+//takean input of age either if it would be greater than 18 than alert too old otherwise too yooung/
+var inp = prompt('enter your age')
+var math=true
+for ( var i=0; i<=18; i++){
+    if(inp==i){
+        document.write('you are too young')
+    }
+}
+
+    for ( var j=18; j>18; j++){
+
+         if(inp==j){
+            document.write('you are too old')
+        } 
+
+    }
+
+        
+
+    
+    
